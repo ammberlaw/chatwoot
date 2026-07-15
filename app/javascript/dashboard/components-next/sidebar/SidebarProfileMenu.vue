d46b9bcdabc607edc87bbb6975a08fd95161f209
Amber Law <hiamberlaw@gmail.com>
2026-07-16 05:47:30 +0800

    feat(crm): remove docs, changelog and appearance from profile menu

--- app/javascript/dashboard/components-next/sidebar/SidebarProfileMenu.vue
+++ app/javascript/dashboard/components-next/sidebar/SidebarProfileMenu.vue
@@ -111,34 +111,6 @@ const menuItems = computed(() => {
       click: openAvatarPicker,
     },
     {
-      show: true,
-      showOnCustomBrandedInstance: true,
-      label: t('SIDEBAR_ITEMS.APPEARANCE'),
-      icon: 'i-lucide-palette',
-      click: () => {
-        const ninja = document.querySelector('ninja-keys');
-        ninja.open({ parent: 'appearance_settings' });
-      },
-    },
-    {
-      show: true,
-      showOnCustomBrandedInstance: false,
-      label: t('SIDEBAR_ITEMS.DOCS'),
-      icon: 'i-lucide-book',
-      link: 'https://www.chatwoot.com/hc/user-guide/en',
-      nativeLink: true,
-      target: '_blank',
-    },
-    {
-      show: true,
-      showOnCustomBrandedInstance: false,
-      label: t('SIDEBAR_ITEMS.CHANGELOG'),
-      icon: 'i-lucide-scroll-text',
-      link: 'https://www.chatwoot.com/changelog/',
-      nativeLink: true,
-      target: '_blank',
-    },
-    {
       show: currentUser.value.type === 'SuperAdmin',
       showOnCustomBrandedInstance: true,
       label: t('SIDEBAR_ITEMS.SUPER_ADMIN_CONSOLE'),
